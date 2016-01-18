bae2252b17ffdc936baa3f6c115bc183a2ffaae9
Roger Hutchings <roger@hutchin.gs>
2016-01-18 18:57:51 +0000

    Move get_collaborator to top

--- panoptes_cli/scripts/panoptes.py
+++ panoptes_cli/scripts/panoptes.py
@@ -1,14 +1,29 @@
-import click
 from multiprocessing import Pool
 
+import click
 from panoptes_client.panoptes import Panoptes
+
 
 pool = Pool(processes=4)
 panoptes = Panoptes('https://panoptes.zooniverse.org/api')
 
+
+def get_collaborator(role_id):
+    """Takes a role id, and returns an object containing the user information
+    for that particular role."""
+    role_data = panoptes.get_project_role(role_id)['project_roles'][0]
+    user_data = panoptes.get_user(role_data['links']['owner']['id'])['users'][0]
+    return {
+        'id': user_data['id'],
+        'login': user_data['login'],
+        'roles': ', '.join(role_data['roles']),
+    }
+
+
 @click.group()
 def cli():
     pass
+
 
 @cli.command()
 @click.option('--id', help='Project ID', required=False, type=int)
@@ -22,14 +37,17 @@ def project(id, display_name, slug, sets, roles, verbose):
 
     for proj_data in projects['projects']:
         click.echo('Project name: %s' % proj_data['display_name'])
-        click.echo('\tClassification count: %s' % proj_data['classifications_count'])
+        click.echo('\tClassification count: %s' %
+                   proj_data['classifications_count'])
         click.echo('\tSubject count: %s' % proj_data['subjects_count'])
         click.echo('')
 
         if sets or verbose:
             click.echo('Subject sets:')
 
-            subject_sets = pool.map(lambda id: panoptes.get_subject_set(id)['subject_sets'][0], proj_data['links']['subject_sets'])
+            subject_sets = map(lambda id: panoptes.get_subject_set(id)
+                               ['subject_sets'][0],
+                               proj_data['links']['subject_sets'])
 
             for subject_set in subject_sets:
                 click.echo('\t%(id)s: %(display_name)s' % subject_set)
@@ -39,19 +57,12 @@ def project(id, display_name, slug, sets, roles, verbose):
         if roles or verbose:
             click.echo('Roles:')
 
-            def get_collaborator(role_id):
-                role_data = panoptes.get_project_role(role_id)['project_roles'][0]
-                user_data = panoptes.get_user(role_data['links']['owner']['id'])['users'][0]
-                return {
-                    'id': user_data['id'],
-                    'login': user_data['login'],
-                    'roles': ', '.join(role_data['roles']),
-                }
-
-            collaborators = pool.map(get_collaborator, proj_data['links']['project_roles'])
+            collaborators = pool.map(get_collaborator, proj_data['links']
+                                     ['project_roles'])
 
             for collaborator in collaborators:
                 click.echo('\t%(login)s (%(id)s): %(roles)s' % collaborator)
+
 
 @cli.command()
 @click.argument('subject_id', required=True, type=int)
