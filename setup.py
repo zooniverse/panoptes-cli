4ba040182ceddd8dced8fca7e3b6692fad18d2a6
Adam McMaster <adam@zooniverse.org>
2016-06-17 10:02:12 +0100

    Don't try to set long_description from README.md
    Fails on install...

--- setup.py
+++ setup.py
@@ -4,16 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='panoptescli',
-    version='0.1',
+    version='0.1.1',
     url='https://github.com/zooniverse/panoptes-cli',
     author='Adam McMaster',
     author_email='adam@zooniverse.org',
     description=(
         'A command-line client for Panoptes, the API behind the Zooniverse'
     ),
-    long_description=open(
-        os.path.join(os.path.dirname(__file__), 'README.md')
-    ).read(),
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
