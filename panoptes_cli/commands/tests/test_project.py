da544627714d8d6078a2f01387cf9d1bbe5868ff
Adam McMaster <adam@12mtns.com>
2016-10-06 14:56:18 -0500

    Set up unit tests (#10)

---
+++ panoptes_cli/commands/tests/test_project.py
@@ -0,0 +1,32 @@
+from click.testing import CliRunner
+from panoptes_cli.commands.project import ls
+
+import unittest
+
+
+class TestProject(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestProject, self).__init__(*args, **kwargs)
+        self.runner = CliRunner()
+
+    def test_ls_id_public(self):
+        result = self.runner.invoke(ls, ['--project-id', '1'])
+        self.assertEqual(
+            result.output,
+            '1 zooniverse/snapshot-supernova Snapshot Supernova\n'
+        )
+
+    def test_ls_id_private_anon(self):
+        result = self.runner.invoke(ls, ['--project-id', '7'])
+        self.assertEqual(result.output, '')
+
+    def test_ls_slug_public(self):
+        result = self.runner.invoke(ls, ['zooniverse/snapshot-supernova'])
+        self.assertEqual(
+            result.output,
+            '1 zooniverse/snapshot-supernova Snapshot Supernova\n'
+        )
+
+    def test_ls_slug_private_anon(self):
+        result = self.runner.invoke(ls, ['astopy/testing'])
+        self.assertEqual(result.output, '')
