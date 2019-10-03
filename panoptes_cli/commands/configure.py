d62a33dc52a475bbe910e0c8381b1a02f9255ea2
Adam McMaster <adam@zooniverse.org>
2019-10-03 17:04:51 +0100

    Validate endpoint config
    Make sure it's an HTTPS URL.
    
    Also don't modify endpoint by default since most people won't want to.
    
    Closes #96

--- panoptes_cli/commands/configure.py
+++ panoptes_cli/commands/configure.py
@@ -6,13 +6,25 @@ from panoptes_cli.scripts.panoptes import cli
 
 @cli.command()
 @click.pass_context
-def configure(ctx):
+@click.option(
+    '--edit-all',
+    '-a',
+    help=(
+        "Modify all configuration options (rather than just username and "
+        "password)."
+    ),
+    is_flag=True
+)
+def configure(ctx, edit_all):
     """Sets default values for configuration options."""
 
     if not os.path.isdir(ctx.parent.config_dir):
         os.mkdir(ctx.parent.config_dir)
 
     for opt, value in ctx.parent.config.items():
+        if opt == 'endpoint' and not edit_all:
+            continue
+
         is_password = opt == 'password'
         ctx.parent.config[opt] = click.prompt(
             opt,
@@ -20,6 +32,12 @@ def configure(ctx):
             hide_input=is_password,
             show_default=not is_password,
         )
+
+    if not ctx.parent.config['endpoint'].startswith('https://'):
+        click.echo(
+            'Error: Invalid endpoint supplied. Endpoint must be an HTTPS URL.'
+        )
+        return -1
 
     with open(ctx.parent.config_file, 'w') as conf_f:
         yaml.dump(ctx.parent.config, conf_f, default_flow_style=False)
