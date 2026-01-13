40d68f4e38f03f544bef19f3894c630a49ea50fb
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2026-01-13 09:33:51 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,10 +17,10 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click>=6.7,<8.2',
-        'PyYAML>=5.1,<6.1',
+        'Click>=6.7',
+        'PyYAML>=5.1',
         'panoptes-client>=1.7,<2.0',
-        'humanize>=0.5.1,<4.8',
+        'humanize>=0.5.1',
         'pathvalidate>=0.29.0,<2.6',
     ],
     entry_points='''
